33740768d1cb3a4715c2a0291aa5eb2c2ca803f3
anh20923 <anh.vainionpaa@gmail.com>
2026-03-24 00:42:34 +0200

    add video20 files

---
+++ src/video20/video20.js
@@ -0,0 +1,13 @@
+console.log("Video 20")
+
+const a=5; b = 10; c = 15;
+const d = a*b+c;
+console.log(d)
+
+let x = 5;
+//let y = ++x; // ++ => tang ngay lap tuc , a = 6, b = 6
+let z = x--; // 
+
+console.log("x = " + x);
+//console.log("y = " + y);
+console.log("z = " + z);
