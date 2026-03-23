51e79049d98a897123825f02b9960a52d636468d
anh20923 <anh.vainionpaa@gmail.com>
2026-03-24 00:08:13 +0200

    update video18, create video19

--- src/video18/video18.js
+++ src/video18/video18.js
@@ -1,2 +1,25 @@
 console-log("Video 18")
 
+//number
+const myAge = 25;
+const myScore = 9.1;
+console.log(myAge);
+console.log(myScore);
+
+//string
+const name = "Anh";
+const name1 ='Täplä'; // Không nên sử dụng
+const name2 = `Kalle`; 
+console.log(name)
+
+//boolean: true/false
+const isHandsome = true;
+console.log(isHandsome)
+
+//undefined
+let bla;
+console.log(bla);
+
+//null
+let testNull = null;
+console.log(testNull);
---
+++ src/video19/video19.js
@@ -0,0 +1,15 @@
+console.log("video 19")
+
+//object: key-value
+const person = {
+    name: "Anh",
+    age: 25,
+    address: "Saigon"
+}
+
+console.log(person)
+
+
+//array
+const scores = [10, 3, 4, 8, 9]
+console.log(scores)
