f2642a26c6da31744fcb645379590141702f80a6
Anh Tran <anh.vainionpaa@gmail.com>
2026-03-24 18:20:46 +0200

    update lab01

--- src/lab01/lab01.js
+++ src/lab01/lab01.js
@@ -1,1 +1,16 @@
 console.log("Lab 01")
+
+let fullName = "Tran My Anh";
+let birthYear = 1994
+let isStudent = true;
+
+const today = new Date();
+const currentYear = today.getFullYear()
+
+const info = 
+`Full name: ${fullName}
+Age: ${currentYear - birthYear}
+Student: ${isStudent}
+`
+
+console.log(info)
