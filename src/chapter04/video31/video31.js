1e25af9b3b2b22e4f29a6ccf5d75e1ba5bbe2b5d
Anh Tran <anh.vainionpaa@gmail.com>
2026-03-25 22:31:19 +0200

    create folders video 31, 32, 33

---
+++ src/chapter04/video31/video31.js
@@ -0,0 +1,16 @@
+console.log("Video31")
+
+// function sum(a,b){
+//     return a+b
+// }
+
+const sum = (a,b) => {
+    return a + b;
+}
+
+
+console.log(sum(45,5));
+
+// (function () {
+//     console.log("chay ngay di")
+// })();
