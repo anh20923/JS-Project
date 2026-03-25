ac4cbec209566de65db74e5b03ca923572cd007d
Anh Tran <anh.vainionpaa@gmail.com>
2026-03-25 20:32:06 +0200

    update chapter4

---
+++ src/chapter04/video26/video26.js
@@ -0,0 +1,16 @@
+console.log("Video 26")
+
+// if / else / else if
+//score => A, B, C, D, F. Giỏi, Khá, Trung Bình
+const score = 4;
+if (score > 9) {
+    console.log("Giỏi")
+}
+
+else if (score > 6 && score < 9) {
+    console.log("Khá")
+}
+
+else {
+    console.log("Trung Bình")
+}
