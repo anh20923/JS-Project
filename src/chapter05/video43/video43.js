9b3952d2b7f2e52f0d4add107dc9c387db752f12
Anh Tran <anh.vainionpaa@gmail.com>
2026-03-26 23:50:14 +0200

    video43

---
+++ src/chapter05/video43/video43.js
@@ -0,0 +1,16 @@
+console.log("Video 43")
+
+const person = {
+    name1: "hoidanit",
+    age: 25
+}
+
+console.log("person before: ", person);
+
+//get data
+console.log("age: ", person.age);
+console.log("name: ", person["name1"]) 
+
+//adding more info - set data
+person.address ="saigon"
+console.log("person after: ", person)
