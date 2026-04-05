39e30dcf01ca53dda7e5e9d50a7c3cd0e6a533b3
Anh Tran <anh.vainionpaa@gmail.com>
2026-04-05 19:33:42 +0300

    video49

---
+++ src/chapter06/video49/video49.js
@@ -0,0 +1,13 @@
+console.log("Video 49!")
+
+//getElementById
+const userName = document.getElementById("username")
+// console.log(userName)
+
+//querySelector
+const text = document.querySelector(".text")
+// console.log(text)
+
+//querySelectorAll
+const allTexts = document.querySelectorAll(".text")
+console.log(allTexts)
