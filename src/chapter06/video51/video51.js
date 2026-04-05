f05f5df4f8ecd1a0732f71bd84e638bcd7210cac
Anh Tran <anh.vainionpaa@gmail.com>
2026-04-05 20:41:38 +0300

    video51

---
+++ src/chapter06/video51/video51.js
@@ -0,0 +1,10 @@
+console.log("Video51")
+
+const element = document.getElementById("myBtn")
+//console.log(element)
+
+const handleClickBtn = () => {
+    console.log("you click a button")
+}
+
+element.addEventListener("click", handleClickBtn)
