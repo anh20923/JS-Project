61d58410336911fa5b86248287d00ad45b667a12
Anh Tran <anh.vainionpaa@gmail.com>
2026-04-28 14:37:05 +0300

    lab05

---
+++ src/chapter07/lab05/lab05.js
@@ -0,0 +1,23 @@
+console.log("lab 05")
+
+
+const fetchUsers = async() => {
+    const res = await fetch("http://localhost:8000/users")
+    const data = await res.json()
+    console.log(data)
+    
+    if(data && data.length){
+        const tbody = document.querySelector("#users tbody");
+        data.forEach((user, index) => {
+            tbody.innerHTML += `
+            <tr>
+                <td>${user.id}</td>
+                <td>${user.name}</td>
+                <td>${user.email}</td>
+            </tr>`
+        });
+        
+    }
+}
+
+fetchUsers()
