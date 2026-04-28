5b8e20e2fcdb650450f9d6b31d136e34fd01a46b
Anh Tran <anh.vainionpaa@gmail.com>
2026-04-28 16:37:36 +0300

    video55

--- src/chapter06/video55/video55.js
+++ src/chapter06/video55/video55.js
@@ -1,17 +1,17 @@
 console.log("video 55")
 
 const btn = document.getElementById("submitBtn")
-const input = document.getElementById("name")
+const input = document.getElementById("inputName")
 
-const prevElement = document.getElementById("prev")
-const prevName = localStorage.getItem("name")
+const prevElement = document.getElementById("previousName")
+const prevName = localStorage.getItem("inputName")
 if(prevName){
-    prevElement.innerHTML = `<b>${prevName}</b>`
+    prevElement.innerHTML = `<b>${prevName}</b>`;
 }
 
 btn.addEventListener("click", () => {
     //console.log(input.value)
-    localStorage.setItem("name", input.value)
-    document.getElementById("message").innerHTML = `<b>${input.value}</b>`
+    localStorage.setItem("inputName", input.value)
+    document.getElementById("currentName").innerHTML = `<b>${input.value}</b>`;
 
 })
