ce7fd7cb7a9234aa1d5fd52907b84067ce51a065
Anh Tran <anh.vainionpaa@gmail.com>
2026-05-06 00:00:05 +0300

    update CSS style for blogs table

--- src/chapter08/video72/video72.js
+++ src/chapter08/video72/video72.js
@@ -1,1 +1,8 @@
 console.log("video72")
+
+const fetchBlogs = async() => { 
+    const res = await fetch("http://localhost:8000/blogs")
+    const data = await res.json();
+    console.log(data)
+
+}
